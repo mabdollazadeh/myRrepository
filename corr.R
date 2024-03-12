b6eb59f1d172ad3540af7372c9b34fded68081c5
mabdollazadeh <m.abdollazadeh@gmail.com>
2024-03-12 21:10:34 +0330

    correlation

---
+++ corr.R
@@ -0,0 +1,18 @@
+corr <- function(directory,threshold=0){
+j <- 1;
+for(i in 1:332){
+fpath <- paste(directory,"/",sprintf("%03d",i),".csv",sep="")
+tmp <- read.csv(fpath,header=T)
+cc <- tmp[complete.cases(tmp), ]
+nr <- nrow(cc)
+if(nr>threshold & j==1){
+dfp <- data.frame(cc)
+cr <- cor(dfp[,2],dfp[,3])
+j <- j+1
+}else if(nr>threshold & j>1){
+dfp <- data.frame(cc)
+cr <- rbind(cr,cor(dfp[,2],dfp[,3]))
+}
+}
+cr
+}
