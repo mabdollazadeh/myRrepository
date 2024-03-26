03903a3ab6c6061fa403e633cbae7104762db8bf
mabdollazadeh <m.abdollazadeh@gmail.com>
2024-03-26 21:16:18 +0330

    clean dataset

--- gettingdata/script.R
+++ gettingdata/script.R
@@ -1,3 +1,4 @@
+#merge
 traindf <- read.table("D:/DS/Project/gettingdata/UCI HAR Dataset/train/X_train.txt")
 dim(traindf)
 library(data.table)
@@ -19,53 +20,30 @@ dim(testtrainLables)
 sublabledata <- cbind(testtrainSubjects,testtrainLables)
 sublabledata <- cbind(sublabledata,testtraindt)
 dim(sublabledata)
+#extract
 features <- fread("D:/DS/Project/gettingdata/UCI HAR Dataset/features.txt")
-grep("(.*)mean|(.*)std",features)
-length(features)
-features
-dim(features)
-features[1,1]
-grep("(.*)mean|(.*)std",features[,2])
-featurnames <- features[,2]
-featurenames
-head(featurnames)
-grep("(.*)mean|(.*)std",featurenames)
-grep("(.*)mean|(.*)std",featurnames)
-grep("mean|std",featurnames)
-found <- grep("mean|std",featurnames)
-found
-dim(featurnames)
-featurename[1]
-featurnames[1]
-found <- grep("mean|std",featurnames[,1])
-length(found)
-found <- grep("mean|std",featurnames[])
-length(found)
-tables()
-splitnames <- strsplit(featurnames,"()")
-splitnames <- strsplit(featurnames,"\(\)")
-subnames <- gsub("-","",featurnames)
-dim(subnames)
-length(subnames)
-subnames
-setnames(featurnames,names(featurnames),"featureName")
-grep("mean|std",featurnames$featureName)
-setnames(featurnames,names(features),c("featureNumber,"featureName"))
-setnames(featurnames,names(features),c("featureNumber","featureName"))
-names(features)
 setnames(features,names(features),c("featureNumber","featureName"))
-features <- features[grepl("mean|std",featurnames$featureName)]
-dim(features)
-extrdt <- sublabledata[,features[,2]]
-dim(extrdt)
-features[,2]
-features[,1]
-extrdt <- sublabledata[,features[,1]]
-dim(extrdt)
-extrdt <- subset(sublabledata,,features[,1])
-extrdt <- subset(sublabledata,,features[,1],with=T)
-extrdt <- subset(sublabledata,,features[,1])
-dim(extrdt)
-dim(features)
-features[10,]
+features <- features[grepl("mean|std",features$featureName)]
+names(sublabledata)
+colnames(sublabledata)[1] <- "subjectid"
+colnames(sublabledata)[2] <- "activitylable"
+selectcols <- features[,featureNumber]
+colsdf <- data.frame(selectcols)
+colsdf$selectcols=paste0("V",colsdf$selectcols)
+head(colsdf,n=10)
+dim(sublabledata)
+str(colsdf)
+chcols <- as.character(colsdf[,1])
+length(chcols)
+chcols <- c("subjectid","activitylable",chcols)
+length(chcols)
+head(chcols)
+res <- sublabledata[,..chcols]
+dim(res)
+str(res)
+#descriptive act names
+actlablesdf <- read.table("D:/DS/Project/gettingdata/UCI HAR Dataset/activity_labels.txt")
+res[,activitylable:=actlablesdf[activitylable,2]]
+#descriptive label dataset
+
 
